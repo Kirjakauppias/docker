baaf981b3d37dc70c1bfee3770ea7c4767825e17
Kirjakauppias <128515579+Kirjakauppias@users.noreply.github.com>
2024-08-05 19:53:28 +0300

    Docker

---
+++ docker_test/src/eka_HTMX/index.php
@@ -0,0 +1,20 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Eka HTMX</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <header id="main-header">
+        <img src="htmx_logo.png" alt="HTMX logo">
+        <h1>high power tools for HTML</h1>
+    </header>
+    <main>
+        <p>You can build modern uesr intrefaces with the </p>
+        <button>
+            Learn more
+        </button>
+    </main>
+</body>
+</html>
